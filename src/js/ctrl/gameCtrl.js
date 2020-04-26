fbf93ae46bf4bacf30a46d80e635a832e546cd1b
Lennert Dael <lennert.dael@live.com>
2020-04-26 06:19:33 +0200

    ssl

--- src/js/ctrl/gameCtrl.js
+++ src/js/ctrl/gameCtrl.js
@@ -571,8 +571,8 @@ class PlayerScreen{
 
 class IO {
     constructor() {
-        //this.socket = new ServerEventsDispatcher('wss://qwizz-socket.herokuapp.com');
-        this.socket = new ServerEventsDispatcher('ws://localhost:3000');
+        this.socket = new ServerEventsDispatcher('wss://kwispel.herokuapp.com');
+        //this.socket = new ServerEventsDispatcher('ws://localhost:3000');
         this.bindEvents();
         this.socketId;
     }
