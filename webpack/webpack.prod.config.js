a8e6edaa90205cb2ee278d6448df2879f43599ba
dleemans <dleemans@itg.be>
2021-01-01 13:07:47 +0100

    .

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -5,7 +5,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     main: './src/js/app.js'
   },
